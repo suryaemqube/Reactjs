8dec7a8939f4374cda2aaf41ea4e3204580b8391
emqubedev1 <emqubedev1@Emqubeindia.local>
2023-06-13 18:43:26 +0530

    third init

--- src/templates/CustomPostDetail.js
+++ src/templates/CustomPostDetail.js
@@ -12,16 +12,23 @@ const CustomPostDetail = ({ pageContext }) => {
                 <h2>{post.title}</h2>
                 <img src={post.featuredImage.node.mediaItemUrl} alt={post.title} />
                 <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                <div>
-                    <h2>ACF Fields</h2>
-                    {acf.map((insight, index) => (
-                        <div key={index}>
-                            <h3>{insight.digitallyName}</h3>
-                            <img src={insight.digitallyImage.mediaItemUrl} alt={insight.digitallyName} />
-                            <p>{insight.digitallyDescription}</p>
-                        </div>
-                    ))}
-                </div>
+
+                {acf === null ? (
+                    null
+                ) : (
+                    <div>
+
+                        <h2>ACF Fields</h2>
+                        {acf.map((insight, index) => (
+                            <div key={index}>
+                                <h3>{insight.digitallyName}</h3>
+                                <img src={insight.digitallyImage.mediaItemUrl} alt={insight.digitallyName} />
+                                <p>{insight.digitallyDescription}</p>
+                            </div>
+                        ))}
+
+                    </div>
+                )}
             </div>
         </Layout>
     );
